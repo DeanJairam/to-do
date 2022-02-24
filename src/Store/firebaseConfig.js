0aefe9f832215185cc023a83fe5ee2ef4bea0489
DeanJairam <76832736+DeanJairam@users.noreply.github.com>
2022-02-24 19:35:43 +0530

    Add files via upload

---
+++ src/Store/firebaseConfig.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyB_f8lc2VDV4YbjCy7KGrlt0AX0dc5HFzQ",
+    authDomain: "to-do-15be2.firebaseapp.com",
+    projectId: "to-do-15be2",
+    storageBucket: "to-do-15be2.appspot.com",
+    messagingSenderId: "909729047068",
+    appId: "1:909729047068:web:a6b0662b2b7daae485b5d9",
+  };
---
+++ src/pages/Create.svelte
@@ -0,0 +1,93 @@
+<script>
+  import { Link } from "svelte-routing";
+  import { navigate } from "svelte-routing";
+  import { getFirestore, collection, addDoc } from "firebase/firestore";
+
+  const db = getFirestore();
+
+  let error = false;
+
+  let title = "";
+  let description = "";
+  let isComplete = false;
+
+  const handleSubmit = async () => {
+    if (title.length > 2) {
+      const colRef = await addDoc(collection(db, "tasks"), {
+        title,
+        description,
+        isComplete,
+      });
+      navigate("/");
+    } else {
+      error = true;
+    }
+  };
+</script>
+
+<main>
+  <h2>Create task</h2>
+  <form on:submit|preventDefault={handleSubmit}>
+    <input
+      class="name-input"
+      type="text"
+      placeholder="Name"
+      bind:value={title}
+    />
+    {#if error == true}
+      <p>ℹ Enter the task with minimum of 3 characters</p>
+    {/if}
+    <br />
+    <textarea placeholder="Description(optional)" bind:value={description} />
+  </form>
+  <section>
+    <button id="add-btn" on:click={handleSubmit}>Add</button>
+    <Link to="/"><button id="cancel-btn">Cancel</button></Link>
+  </section>
+</main>
+
+<style>
+  form {
+    display: flex;
+    flex-direction: column;
+  }
+  form,
+  section {
+    margin: 0 10vw;
+  }
+
+  textarea {
+    height: 10vh;
+  }
+  section {
+    display: flex;
+    justify-content: center;
+    padding: 20px 0;
+  }
+  p {
+    color: #ff0000;
+  }
+
+  button {
+  background-color: var(--button-color);
+  border: 2px solid var(--button-color);
+  border-radius: 10px;
+  font-family: var(--font);
+  font-weight: bold;
+  font-size: 24px;
+  text-align: center;
+  width: 10rem;
+  padding: 1rem;
+  margin: 0 5px;
+}
+  #cancel-btn {
+    background-color: #f1fff8;
+    border-color: #004221;
+  }
+  @media only screen and (max-width: 425px) {
+    form,
+    section {
+      margin: 0 6vw;
+    }
+  }
+</style>
